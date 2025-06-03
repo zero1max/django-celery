de9e39c6137dd5c5b37087c74bf13f6098ee693b
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-06-03 11:08:40 +0500

    update

--- core/settings.py
+++ core/settings.py
@@ -71,6 +71,8 @@ DATABASES = {
     }
 }
 
+TELEGRAM_BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
 CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
 
